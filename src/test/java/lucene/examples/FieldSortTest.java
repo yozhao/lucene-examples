29bc28e4b0aca1a326a76ba2634b7739ea791851
Yonghui Zhao <zhaoyonghui@gmail.com>
2014-06-06 01:26:07 +0800

    Update FieldSort example

--- src/test/java/lucene/examples/FieldSortTest.java
+++ src/test/java/lucene/examples/FieldSortTest.java
@@ -57,14 +57,95 @@ public class FieldSortTest extends TestCase {
 
   @Test
   public void testLongSort() throws Exception {
-    // TODO
+    IndexSearcher searcher = new IndexSearcher(reader);
+    // Sort all docs
+    Sort sort = new Sort(new SortField("long", SortField.Type.LONG));
+    Query q = new MatchAllDocsQuery();
+    TopDocs docs = searcher.search(q, 10, sort);
+    assertEquals(10, docs.totalHits);
+    assertEquals("8", searcher.doc(docs.scoreDocs[0].doc).get("id"));
+    assertEquals("0", searcher.doc(docs.scoreDocs[1].doc).get("id"));
+    assertEquals("2", searcher.doc(docs.scoreDocs[2].doc).get("id"));
+    assertEquals("1", searcher.doc(docs.scoreDocs[3].doc).get("id"));
+    assertEquals("3", searcher.doc(docs.scoreDocs[4].doc).get("id"));
+    assertEquals("7", searcher.doc(docs.scoreDocs[5].doc).get("id"));
+    assertEquals("4", searcher.doc(docs.scoreDocs[6].doc).get("id"));
+    assertEquals("6", searcher.doc(docs.scoreDocs[7].doc).get("id"));
+    assertEquals("5", searcher.doc(docs.scoreDocs[8].doc).get("id"));
+    assertEquals("9", searcher.doc(docs.scoreDocs[9].doc).get("id"));
+    // Sort docs in a range
+    q = TermRangeQuery.newStringRange("string", "abcd", "abcdef", true, true);
+    docs = searcher.search(q, 10, sort);
+    assertEquals(6, docs.totalHits);
+    assertEquals("8", searcher.doc(docs.scoreDocs[0].doc).get("id"));
+    assertEquals("2", searcher.doc(docs.scoreDocs[1].doc).get("id"));
+    assertEquals("3", searcher.doc(docs.scoreDocs[2].doc).get("id"));
+    assertEquals("4", searcher.doc(docs.scoreDocs[3].doc).get("id"));
+    assertEquals("6", searcher.doc(docs.scoreDocs[4].doc).get("id"));
+    assertEquals("5", searcher.doc(docs.scoreDocs[5].doc).get("id"));
   }
 
   @Test
   public void testDoubleSort() throws Exception {
-    // TODO
+    IndexSearcher searcher = new IndexSearcher(reader);
+    // Sort all docs
+    Sort sort = new Sort(new SortField("double", SortField.Type.DOUBLE));
+    Query q = new MatchAllDocsQuery();
+    TopDocs docs = searcher.search(q, 10, sort);
+    assertEquals(10, docs.totalHits);
+    assertEquals("6", searcher.doc(docs.scoreDocs[0].doc).get("id"));
+    assertEquals("5", searcher.doc(docs.scoreDocs[1].doc).get("id"));
+    assertEquals("0", searcher.doc(docs.scoreDocs[2].doc).get("id"));
+    assertEquals("3", searcher.doc(docs.scoreDocs[3].doc).get("id"));
+    assertEquals("7", searcher.doc(docs.scoreDocs[4].doc).get("id"));
+    assertEquals("2", searcher.doc(docs.scoreDocs[5].doc).get("id"));
+    assertEquals("1", searcher.doc(docs.scoreDocs[6].doc).get("id"));
+    assertEquals("4", searcher.doc(docs.scoreDocs[7].doc).get("id"));
+    assertEquals("8", searcher.doc(docs.scoreDocs[8].doc).get("id"));
+    assertEquals("9", searcher.doc(docs.scoreDocs[9].doc).get("id"));
+    // Sort docs in a range
+    q = TermRangeQuery.newStringRange("string", "abcd", "abcdef", true, true);
+    docs = searcher.search(q, 10, sort);
+    assertEquals(6, docs.totalHits);
+    assertEquals("6", searcher.doc(docs.scoreDocs[0].doc).get("id"));
+    assertEquals("5", searcher.doc(docs.scoreDocs[1].doc).get("id"));
+    assertEquals("3", searcher.doc(docs.scoreDocs[2].doc).get("id"));
+    assertEquals("2", searcher.doc(docs.scoreDocs[3].doc).get("id"));
+    assertEquals("4", searcher.doc(docs.scoreDocs[4].doc).get("id"));
+    assertEquals("8", searcher.doc(docs.scoreDocs[5].doc).get("id"));
   }
 
+  @Test
+  public void testCombinedSort() throws Exception {
+    IndexSearcher searcher = new IndexSearcher(reader);
+    // Sort all docs
+    Sort sort = new Sort(new SortField[] { new SortField("string", SortField.Type.STRING),
+        new SortField("double", SortField.Type.DOUBLE) });
+    Query q = new MatchAllDocsQuery();
+    TopDocs docs = searcher.search(q, 10, sort);
+    assertEquals("7", searcher.doc(docs.scoreDocs[0].doc).get("id"));
+    assertEquals("9", searcher.doc(docs.scoreDocs[1].doc).get("id"));
+    assertEquals("0", searcher.doc(docs.scoreDocs[2].doc).get("id"));
+    assertEquals("1", searcher.doc(docs.scoreDocs[3].doc).get("id"));
+    assertEquals("3", searcher.doc(docs.scoreDocs[4].doc).get("id"));
+    assertEquals("2", searcher.doc(docs.scoreDocs[5].doc).get("id"));
+    assertEquals("4", searcher.doc(docs.scoreDocs[6].doc).get("id"));
+    assertEquals("5", searcher.doc(docs.scoreDocs[7].doc).get("id"));
+    assertEquals("8", searcher.doc(docs.scoreDocs[8].doc).get("id"));
+    assertEquals("6", searcher.doc(docs.scoreDocs[9].doc).get("id"));
+    // Sort docs in a range
+    q = TermRangeQuery.newStringRange("string", "abcd", "abcdef", true, true);
+    sort = new Sort(new SortField[] { new SortField("string", SortField.Type.STRING),
+        new SortField("double", SortField.Type.DOUBLE, true) });
+    docs = searcher.search(q, 10, sort);
+    assertEquals(6, docs.totalHits);
+    assertEquals("4", searcher.doc(docs.scoreDocs[0].doc).get("id"));
+    assertEquals("2", searcher.doc(docs.scoreDocs[1].doc).get("id"));
+    assertEquals("3", searcher.doc(docs.scoreDocs[2].doc).get("id"));
+    assertEquals("8", searcher.doc(docs.scoreDocs[3].doc).get("id"));
+    assertEquals("5", searcher.doc(docs.scoreDocs[4].doc).get("id"));
+    assertEquals("6", searcher.doc(docs.scoreDocs[5].doc).get("id"));
+  }
 }
 
 
